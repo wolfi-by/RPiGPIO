6a09452a0b080cc314e9f2b838f1915d6bc39361
Wolfgang Höhne <w.hoehne@outlook.de>
2021-02-26 09:55:25 +0100

    Projektdateien hinzufügen.

---
+++ RPiGPIO/Data/GPIOControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Device.Gpio;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPiGPIO.Data
+{
+    public class GPIOControl
+    {
+        public GpioController controller { get; set; } = new GpioController();
+
+        
+    }
+}
